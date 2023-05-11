000000e7f2d97758d91bd229cf27bb7d60124832
Sergei Bozhko <sergei.bozhko@proton.me>
2023-05-11 17:08:33 +0200

    added auth forms and fake email service

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -127,3 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "posts:index"
+# LOGOUT_REDIRECT_URL = "posts:index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
