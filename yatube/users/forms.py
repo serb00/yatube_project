a2a173c2ffb7229702ce73a5a1b3773f9a766697
Sergei Bozhko <sergei.bozhko@proton.me>
2023-05-14 11:24:35 +0200

    added contact form, model and view function

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import Contact
 
 User = get_user_model()
 
@@ -12,3 +14,9 @@ class CreationForm(UserCreationForm):
         model = User
         # set up fields and their order on the form
         fields = ("first_name", "last_name", "username", "email")
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ("name", "email", "subject", "body")
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
 # import CreateView to inherit from it
+from django.shortcuts import render
 from django.views.generic import CreateView
 # reverse_lazy allow to get URL by "name" parameter
 from django.urls import reverse_lazy
 # import form class to use it in view class
-from .forms import CreationForm
+from .forms import ContactForm, CreationForm
 
 
 # Create your views here.
@@ -14,3 +15,9 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")  # where login is a parameter in path()
     template_name = "signup.html"
+
+
+def user_contact(request):
+    form = ContactForm()
+
+    return render(request, 'users/contact.htms', {"form": form})
